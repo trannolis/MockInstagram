c7e14483e69c9a41ca9e9df007a4234164d15b39
trannolis <tranno0lis@gmail.com>
2020-04-07 17:50:29 -0400

    Update init1.py

--- init1.py
+++ init1.py
@@ -2,6 +2,9 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 
+import mysql.connector
+from mysql.connector import Error
+
 #Initialize the app from Flask
 app = Flask(__name__)
 
@@ -39,7 +42,7 @@ def loginAuth():
     #cursor used to send queries
     cursor = conn.cursor()
     #executes query
-    query = 'SELECT * FROM user WHERE username = %s and password = %s'
+    query = 'SELECT * FROM Person WHERE username = %s and password = %s'
     cursor.execute(query, (username, password))
     #stores the results in a variable
     data = cursor.fetchone()
@@ -62,11 +65,14 @@ def registerAuth():
     #grabs information from the forms
     username = request.form['username']
     password = request.form['password']
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    email = request.form['email']
 
     #cursor used to send queries
     cursor = conn.cursor()
     #executes query
-    query = 'SELECT * FROM user WHERE username = %s'
+    query = 'SELECT * FROM Person WHERE username = %s'
     cursor.execute(query, (username))
     #stores the results in a variable
     data = cursor.fetchone()
@@ -77,25 +83,25 @@ def registerAuth():
         error = "This user already exists"
         return render_template('register.html', error = error)
     else:
-        ins = 'INSERT INTO user VALUES(%s, %s)'
-        cursor.execute(ins, (username, password))
+        ins = 'INSERT INTO Person VALUES(%s, %s, %s, %s, %s)'
+        cursor.execute(ins, (username, password, firstname, lastname, email))
         conn.commit()
         cursor.close()
         return render_template('index.html')
-
 
 @app.route('/home')
 def home():
     user = session['username']
     cursor = conn.cursor();
-    query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
+    query = 'SELECT postingDate, filePath FROM Photo WHERE poster = %s ORDER BY postingDate DESC'
     cursor.execute(query, (user))
     data = cursor.fetchall()
     cursor.close()
     return render_template('home.html', username=user, posts=data)
 
-        
-@app.route('/post', methods=['GET', 'POST'])
+
+    """
+    @app.route('/post', methods=['GET', 'POST'])
 def post():
     username = session['username']
     cursor = conn.cursor();
@@ -105,6 +111,8 @@ def post():
     conn.commit()
     cursor.close()
     return redirect(url_for('home'))
+    """
+
 
 @app.route('/select_blogger')
 def select_blogger():
@@ -133,8 +141,101 @@ def show_posts():
 def logout():
     session.pop('username')
     return redirect('/')
-        
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+def insertBLOB(username, photo):
+    print("Inserting BLOB into photo table")
+
+    username = request.form['username']
+    postingDate = request.form['postingDate']
+    allFollowers = request.form['allFollowers']
+    caption = request.form['caption']
+
+    #   Just to clarify since you specify that the table auto increments photoIDs
+    #   You do not have to insert into that column since that will be done for you.
+
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='mockinstagram',
+                                             user='root',
+                                             password='root')
+
+        cursor = connection.cursor()
+
+        sql_insert_blob_query = """ INSERT INTO Photo
+                          (postingDate, filePath, allFollowers, caption, poster) VALUES (%s,%s,%s,%s,%s)"""
+
+        photo = convertToBinaryData(photo)
+
+        # Convert data into tuple format
+        insert_blob_tuple = (postingDate, photo, allFollowers, caption, username)
+        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+        connection.commit()
+        print("Image and file inserted successfully as a BLOB into Photos table", result)
+
+    except mysql.connector.Error as error:
+        print("Failed inserting BLOB data into MySQL table {}".format(error))
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+################################################################
+# Retrieving Image and File stored as a BLOB from MySQL Table
+################################################################
+
+def write_file(data, filename):
+    # Convert binary data to proper format and write it on Hard Disk
+    with open(filename, 'wb') as file:
+        file.write(data)
+
+def readBLOB(photoId, photo):
+    print("Reading BLOB data from Photo table")
+
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='mockinstagram',
+                                             user='root',
+                                             password='root')
+
+        cursor = connection.cursor()
+        sql_fetch_blob_query = """SELECT photo from Photo where id = %s"""
+
+        cursor.execute(sql_fetch_blob_query, (photoId,))
+        record = cursor.fetchall()
+        for row in record:
+            print("photoID = ", row[0], )
+            image = row[1]
+            print("Storing photo on disk \n")
+            write_file(image, photo)
+
+    except mysql.connector.Error as error:
+        print("Failed to read BLOB data from MySQL table {}".format(error))
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+
+@app.route('/upload', methods = ['POST'])
+def upload():
+    user = session['username']
+    upfile = request.files['inputFile']
+    insertBLOB(username=user, photo=upfile)
+
+    return "success!" + redirect(url_for('home'))
+
 app.secret_key = 'some key that you will never guess'
+
 #Run the app on localhost port 5000
 #debug = True -> you don't have to restart flask
 #for changes to go through, TURN OFF FOR PRODUCTION
